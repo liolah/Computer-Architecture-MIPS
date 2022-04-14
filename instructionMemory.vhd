9cf16f522fb4da7e6ede54b96f41999c94681c11
Hesham Hany <hesham.h3886@gmail.com>
2022-04-15 00:28:40 +0200

    Update instructionMemory.vhd

--- instructionMemory.vhd
+++ instructionMemory.vhd
@@ -12,22 +12,22 @@ END instructionMemory;
 ARCHITECTURE rtl OF instructionMemory IS
 	TYPE RAM_16_x_32 IS ARRAY(0 TO 15) OF STD_LOGIC_VECTOR(7 DOWNTO 0);
 	SIGNAL IM : RAM_16_x_32 := (
-		x"00",
-		x"00",
-		x"00",
-		x"00",
-		x"00",
-		x"00",
-		x"00",
-		x"00",
-		x"00",
-		x"00",
-		x"00",
-		x"00",
-		x"00",
-		x"00",
-		x"00",
-		x"00"
+		"00000000",
+		"00000000",
+		"00000000",
+		"00000000",
+		"00000000",
+		"00000000",
+		"00000000",
+		"00000000",
+		"00000000",
+		"00000000",
+		"00000000",
+		"00000000",
+		"00000000",
+		"00000000",
+		"00000000",
+		"00000000"
 	);
 BEGIN
 	instruction <= IM(conv_integer(unsigned(readAddress))) & IM(conv_integer(unsigned(readAddress)) + 1) & IM(conv_integer(unsigned(readAddress)) + 2) & IM(conv_integer(unsigned(readAddress)) + 3);
